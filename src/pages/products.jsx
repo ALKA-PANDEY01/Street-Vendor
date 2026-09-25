c3351c7104a929a4dda2feda274c78cc6d19cfda
Alka Pandey <pandeyalka161@gmail.com>
2026-09-25 17:19:49 +0530

    some navbar changes

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -78,7 +78,7 @@ export default function Product({user}){
     return (
         <>
                         <CategoryNavbar selectedCategory={selectedcategory} setSelectedCategory={setSelectedCategory}></CategoryNavbar>
-                <Container className="mt-6 products-page-content" style={{paddingTop:"2rem"}}>
+                <Container className="mt-6 products-page-content">
                         {user && (
                             <h2 className="products-page-heading text-center mt-3">
                                 <WavingHandIcon /> Hello {user.name} !!
